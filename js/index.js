9978e410e3048b1dbdc0cec59bf0f61be68faed2
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-01-19 12:15:04 -0300

    Lista
    Finalizada

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+let contador = 0; 
 let input = document.getElementById('inputTarefa');
 let btnAdd = document.getElementById('btn-add');
 let main = document.getElementById('areaLista');
@@ -8,24 +9,67 @@ function addTarefa(){
 
     //SE NÃO FOR VAZIO, NEM NULO, NEM INDEFINIDO
     if ((valorInput!== "") && (valorInput!== null) && (valorInput!==undefined)){
-       let novoItem = ` <div class="item">
-       <div class="item-icone">
-           <i class="mdi mdi-circle-outline"></i>
+
+       ++contador;
+
+       let novoItem = ` <div id="${contador}" class="item">
+       <div onclick="marcarTarefa(${contador})" class="item-icone">
+           <i id="icone_${contador}" class="mdi mdi-circle-outline"></i>
        </div>
-       <div class="item-nome">
+       <div onclick="marcarTarefa(${contador})" class="item-nome">
            ${valorInput}
        </div>
        <div class="item-botao">
-           <button class="delete"><i class="mdi mdi-delete">Deletar</i></button>
+           <button 
+           onclick="deletar(${contador})" 
+           class="delete"
+           >
+           <i class="mdi mdi-delete">Deletar</i>
+           </button>
+            
        </div>
     </div>`;
-
     //ADICIONAR NOVO ITEM NA MAIN
     main.innerHTML += novoItem;
-
     //ZERAR CAMPO DE TAREFA
     input.value = "";
     input.focus();
 
     }
 }
+
+function deletar(id){
+    var tarefa = document.getElementById(id);
+    tarefa.remove();
+}
+
+function marcarTarefa(id) {
+    var item = document.getElementById(id);
+    var classe = item.getAttribute('class');
+    console.log(classe);
+
+    if(classe=="item") {
+        item.classList.add('clicado');
+
+        var icone = document.getElementById('icone_'+ id);
+        icone.classList.remove('mdi-circle-outline');
+        icone.classList.add('mdi-check-circle');
+
+        item.parentNode.appendChild(item);
+
+    } else {
+        item.classList.remove('clicado');
+
+        var icone = document.getElementById('icone_'+ id);
+        icone.classList.remove('mdi-check-circle');
+        icone.classList.add('mdi-circle-outline');
+    }
+}
+
+input.addEventListener("keyup", function(event){
+    // SE TECLOU ENTER (QUE É RECONHECIDO PELO NUMERO 13) 
+    if (event.keyCode ===13){
+        event.preventDefault();
+        btnAdd.click();
+    }
+})
